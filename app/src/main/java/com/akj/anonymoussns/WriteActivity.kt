92e4ee5f646bb494328361f1749adb5387987d36
akj61300 <rnlwjd0613@gmail.com>
2018-12-18 21:55:47 +0900

    글쓰기 액티비티 유아이 작성

---
+++ app/src/main/java/com/akj/anonymoussns/WriteActivity.kt
@@ -0,0 +1,12 @@
+package com.akj.anonymoussns
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class WriteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_write)
+    }
+}
