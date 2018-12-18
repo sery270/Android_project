46b9a4bea309261e86b2f19cf855ff76161ceb65
akj61300 <rnlwjd0613@gmail.com>
2018-12-18 22:05:23 +0900

    글쓰기 업로드 기능 구현

--- app/src/main/java/com/akj/anonymoussns/MainActivity.kt
+++ app/src/main/java/com/akj/anonymoussns/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.akj.anonymoussns
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     // 로그에 TAG 로 사용할 문자열
@@ -17,23 +15,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // 값의 변경이 있는 경우의 이벤트 리스너를 추가한다.
-        ref.addValueEventListener(object : ValueEventListener {
-            // 데이터 읽기가 취소된 경우 호출 된다.
-            // ex) 데이터의 권한이 없는 경우
-            override fun onCancelled(error: DatabaseError) {
-                error.toException().printStackTrace()
-            }
-
-            // 데이터의 변경이 감지 되면 호출된다.
-            override fun onDataChange(snapshot: DataSnapshot) {
-                // test 키를 가진 데이터 스냅샷에서 값을 읽고 문자열로 변경한다.
-                val message = snapshot.value.toString()
-                // 읽은 문자열을 로깅
-                Log.d(TAG, message)
-                // Firebase 에서 전달받은 메세지로 제목을 변경한다.
-                supportActionBar?.title = message
-            }
-        })
+        // actionbar 의 타이틀을 "글목록" 으로 변경
+        supportActionBar?.title = "글목록"
+        // 하단의 floatingActionButton 이 클릭될때의 리스너를 설정한다.
+        floatingActionButton.setOnClickListener {
+            // Intent 생성
+            val intent = Intent(this@MainActivity, WriteActivity::class.java)
+            // Intent 로 WirteActivity 실행
+            startActivity(intent)
+        }
     }
 }
