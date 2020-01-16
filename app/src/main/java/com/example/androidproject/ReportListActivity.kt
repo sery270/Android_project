aedbefe12b0555003fc1360884b383af0c8a55aa
nahyunnn <wartet2@gamil.com>
2020-01-16 20:31:35 +0900

    firestore 연동2

--- app/src/main/java/com/example/androidproject/ReportListActivity.kt
+++ app/src/main/java/com/example/androidproject/ReportListActivity.kt
@@ -6,32 +6,18 @@ import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.firebase.database.ChildEventListener
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
+
 import kotlinx.android.synthetic.main.activity_report_list.*
 
 
 class ReportListActivity : AppCompatActivity() {
 
-    private var adapter: PostAdapter? = null
-    private var firebase : FirebaseDatabase? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_report_list)
 
 
-
-
-        ReportListActivity_listView.layoutManager = LinearLayoutManager(this)
-        adapter = PostAdapter(this) {
-        }
-        ReportListActivity_listView.adapter = adapter
-
-       // viewDatabase()
     }
 
 
