6c9b35563cf71d3d8a694092a8baccca4793e119
chelinkr18 <chelinkr@naver.com>
2020-01-17 20:18:24 +0900

    Merge branch 'master' of C:\Users\user\Desktop\0106_android\Android_project_new with conflicts.

--- app/src/main/java/com/example/androidproject/LoginActivity.kt
+++ app/src/main/java/com/example/androidproject/LoginActivity.kt
@@ -3,26 +3,74 @@ package com.example.androidproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.example.androidproject.R.id.LoginActivity_login_Button
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
+import java.lang.Exception
 
 class LoginActivity : AppCompatActivity() {
+    private val REQUEST_CODE_SIGN_IN = 1001
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        var inputid = LoginActivity_id_PlainText.text.toString()
-        var inputpasswd = LoginActivity_passwd_Password.text.toString()
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
 
-        LoginActivity_login_Button.setOnClickListener{
+        val googleSignInClient = GoogleSignIn.getClient(this, gso)
 
-            //startActivity(Intent(this,AdminActivity::class.java))
-
-            startActivity(Intent(this,MainActivity::class.java))
+        LoginActivity_login_Button.setOnClickListener {
+            val intent = googleSignInClient.signInIntent
+            startActivityForResult(intent, REQUEST_CODE_SIGN_IN)
         }
+    }
 
-        LoginActivity_join_Button.setOnClickListener{
-            startActivity(Intent(this,JoinActivity::class.java))
+    fun onConnectionFailed(result: ConnectionResult) {
+        Toast.makeText(applicationContext, "연결 실패", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_CODE_SIGN_IN) {
+            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            println(requestCode)
+            println("getin")
+            println(result)
+            Log.v("test",result.toString())
+            println(result.status)
+            if (result.isSuccess) {
+                val account = result.signInAccount
+                val credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+
+                FirebaseAuth.getInstance().signInWithCredential(credential)
+                    .addOnSuccessListener {
+                        Toast.makeText(applicationContext, "인증 성공", Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(applicationContext, MainActivity::class.java))
+                        finish()
+                    }
+                    .addOnFailureListener {
+                        Toast.makeText(applicationContext, "인증 실패(${it.message})", Toast.LENGTH_SHORT).show()
+                    }
+            }
+            else {
+                Toast.makeText(applicationContext, "로그인 실패", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
