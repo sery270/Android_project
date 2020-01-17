701f443c4231c8cd0c4618d3dc2730bf72687eea
nahyunnn <wartet2@gmail.com>
2020-01-17 14:27:06 +0900

    데이터 가져와서 리스트에 뿌리는거 하다만거임

---
+++ app/src/main/java/com/example/androidproject/Adapter.kt
@@ -0,0 +1,56 @@
+package com.example.androidproject
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_report_list.view.*
+import java.util.ArrayList
+
+class Adapter (val context: Context, val itemCheck: (Report) -> Unit)
+    : RecyclerView.Adapter<Adapter.ViewHolder>() {
+    private var items = ArrayList<Report>()
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(viewGroup.context)
+        val itemView: View = inflater.inflate(R.layout.activity_report_list, viewGroup, false)
+        return ViewHolder(itemView, itemCheck)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        val item: Report = items[position]
+        viewHolder.setItem(item)
+    }
+
+    override fun getItemCount(): Int {
+        return items.count()
+    }
+
+    fun setItems(items: ArrayList<Report>) {
+        this.items = items
+    }
+
+    inner class ViewHolder(itemView: View, itemCheck: (Report) -> Unit)
+        : RecyclerView.ViewHolder(itemView) {
+        fun setItem(item: Report) {
+            val resourceId = context.resources.getIdentifier(
+                item.ImageUrl,
+                "drawable",
+                context.packageName
+            )
+            if (resourceId in 0..1) {
+                itemView.uploadimg?.setImageResource(R.mipmap.ic_launcher)
+            }
+            else {
+                itemView.uploadimg?.setImageResource(resourceId)
+            }
+            itemView.poiid.text = item.Id
+            itemView.detail.text = item.DetailLocation
+
+            itemView.setOnClickListener() { itemCheck(item) }
+        }
+    }
+}
--- app/src/main/java/com/example/androidproject/ReportListActivity.kt
+++ app/src/main/java/com/example/androidproject/ReportListActivity.kt
@@ -6,23 +6,50 @@ import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.firestore.FirebaseFirestore
 
 import kotlinx.android.synthetic.main.activity_report_list.*
 
 
 class ReportListActivity : AppCompatActivity() {
 
-
+    private var adapter: Adapter? = null
+    private var firestore : FirebaseFirestore? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_report_list)
 
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = PetAdapter(this) {
+            Toast.makeText(this, "Breed: ${it.breed}, Age: ${it.age}", Toast.LENGTH_SHORT).show()
+        }
+        recyclerView.adapter = adapter
 
+        viewDatabase()
     }
 
-
-
+    private fun viewDatabase() {
+        progressBarView.visibility = View.VISIBLE
+        firestore = FirebaseFirestore.getInstance()
+        firestore?.collection("Pet3")?.get()
+            ?.addOnCompleteListener { task ->
+                progressBarView.visibility = View.GONE
+                if (task.isSuccessful) {
+                    var petList = ArrayList<PetDTO>()
+                    for (dc in task.result!!.documents) {
+                        var petDTO = dc.toObject(PetDTO::class.java)
+                        petList.add(petDTO!!)
+                    }
+                    adapter?.setItems(petList)
+                    adapter?.notifyDataSetChanged()
+                }
+                else {
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
+    }
 }
 
 
