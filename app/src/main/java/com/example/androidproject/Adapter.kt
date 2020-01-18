5094357db845c1f50b6d4c385333e7927a38ba97
jahyun97 <54326150+jahyun97@users.noreply.github.com>
2020-01-18 20:46:22 +0900

    일단 glide 라이브러리 써서 이미지 넣기 시도했는데 될진 모르겠음

--- app/src/main/java/com/example/androidproject/Adapter.kt
+++ app/src/main/java/com/example/androidproject/Adapter.kt
@@ -1,18 +1,22 @@
 package com.example.androidproject
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.net.Uri
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.data_list_item.view.*
 import java.util.ArrayList
 
 class Adapter (val context: Context, val itemCheck: (Report) -> Unit)
     : RecyclerView.Adapter<Adapter.ViewHolder>() {
     private var items = ArrayList<Report>()
+    private var firebaseui:FirebaseUIActivity? = null
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(viewGroup.context)
@@ -36,7 +40,7 @@ class Adapter (val context: Context, val itemCheck: (Report) -> Unit)
     inner class ViewHolder(itemView: View, itemCheck: (Report) -> Unit)
         : RecyclerView.ViewHolder(itemView) {
         fun setItem(item: Report) {
-            val resourceId = context.resources.getIdentifier(
+            /*val resourceId = context.resources.getIdentifier(
                 item.ImageUrl,
                 "drawable",
                 context.packageName
@@ -46,9 +50,16 @@ class Adapter (val context: Context, val itemCheck: (Report) -> Unit)
             }
             else {
                 itemView.uploadimg?.setImageResource(resourceId)
-            }
+            */
+
+
+            // url을 다운받기
+
             itemView.poiid.text = item.Id
             itemView.detail.text = item.DetailLocation
+            firebaseui?.loadWithGlide(item.ImageUrl!!)
+
+
 
             itemView.setOnClickListener() { itemCheck(item) }
         }
