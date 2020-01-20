ea9eca16966d1f9d077cda2b4193f547723c2d01
chelinkr18 <chelinkr@naver.com>
2020-01-20 11:21:20 +0900

    팝업창 성공

--- app/src/main/java/com/example/androidproject/ReportListActivity.kt
+++ app/src/main/java/com/example/androidproject/ReportListActivity.kt
@@ -1,23 +1,22 @@
 package com.example.androidproject
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.ArrayAdapter
-import android.widget.Toast
+import android.view.LayoutInflater
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.firestore.FirebaseFirestore
-
 import kotlinx.android.synthetic.main.activity_report_list.*
-
 
 class ReportListActivity : AppCompatActivity() {
 
     private var adapter: Adapter? = null
     private var firestore : FirebaseFirestore? = null
 
+    var id = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_report_list)
@@ -28,19 +27,53 @@ class ReportListActivity : AppCompatActivity() {
         }
         recyclerview.adapter = adapter
 
-        var id = intent.getStringExtra("id") // 마커클릭시 가져오는 id
+
+        id = intent.getStringExtra("id") // 마커클릭시 가져오는 id
 
         viewDatabase(id)
 
         ReportListActivity_reportbtn_Button.setOnClickListener{
-
             //마커 클릭시 가져온 id를 신고페이지로 넘기기 위함
-            val intent = Intent(this@ReportListActivity,ReportActivity::class.java)
-            val id=intent.putExtra("id",id)
+            var intent = Intent(this@ReportListActivity,ReportActivity::class.java)
+            intent.putExtra("id",id)
 
+            showSettingPopup()
+            //startActivity(intent)
+
+        }
+    }
+
+    private fun showSettingPopup() { //채린
+        val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = inflater.inflate(R.layout.alert_popup, null)
+        val textView1 : TextView = view.findViewById(R.id.alert_popup_textview1)
+        textView1.text = "중복 신고 시 정확한 안전도를 측정하기 어렵습니다."
+        val textView2 : TextView = view.findViewById(R.id.alert_popup_textview2)
+        textView2.text = "다시 한번 목록을 확인한 후 신고해 주세요."
+        val alertDialog = AlertDialog.Builder(this)
+            .setTitle("주의하세요!")
+//            .setPositiveButton("OK") { dialog, which ->
+//                Toast.makeText(applicationContext,"확인하였습니다.",Toast.LENGTH_SHORT).show()
+//                startActivity(intent)
+//            }
+//            .create()
+
+        val butCancel = view.findViewById<Button>(R.id.alert_popup_button1)
+        butCancel.setOnClickListener {//닫기 버튼 눌렀을 때
+            var intent = Intent(this@ReportListActivity,ReportActivity::class.java)
+            intent.putExtra("id",id)
             startActivity(intent)
         }
 
+        alertDialog.setView(view)
+        alertDialog.show()
+        //alertDialog.window.setLayout(500,400)
+
+//        val butSave = view.findViewById<Button>(R.id.alert_popup_button2)
+//        butSave.setOnClickListener {//확인 버튼 눌렀을 때
+//            alertDialog.dismiss()
+////            Toast.makeText(applicationContext,"화면 전환 됐니",Toast.LENGTH_LONG).show()
+//        }
     }
 
     override fun onBackPressed() {
@@ -66,7 +99,6 @@ class ReportListActivity : AppCompatActivity() {
                 }
             }
     }
-
 }
 
 
