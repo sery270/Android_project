3fab5e3a8978ebad094f0ce859e12b4414450c19
nahyunnn <wartet2@gmail.com>
2020-01-20 21:30:45 +0900

    splash

--- app/src/main/java/com/example/androidproject/LoadingActivity.kt
+++ app/src/main/java/com/example/androidproject/LoadingActivity.kt
@@ -1,5 +1,6 @@
 package com.example.androidproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,6 +8,13 @@ class LoadingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_loading)
+
+        try {
+            Thread.sleep(2000)
+        }catch (e: InterruptedException ){
+            e.printStackTrace()
+        }
+
+        startActivity((Intent(this,LoginActivity::class.java)))
     }
 }
